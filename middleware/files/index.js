9e4a046a19a6c0bb46bcb8d41bc9a801b9daa8fa
Michael Haase <darkstar@gernox.de>
2015-03-21 09:56:12 +0100

    Add ability to save files

--- middleware/files/index.js
+++ middleware/files/index.js
@@ -40,6 +40,43 @@ routes.push({
 });
 
 /**
+ * POST /files
+ * Version: 1
+ */
+routes.push({
+    meta: {
+        name: 'saveFile',
+        method: 'POST',
+        paths: [
+            '/files'
+        ],
+        version: '1'
+    },
+    middleware: function(req, res, next) {
+        var fileService = this.services.fileService;
+        var file = req.body.file;
+
+        if (!file) {
+            return next(new restify.BadRequestError("No file to save"));
+        }
+
+        file.createdDate = new Date(file.createdDate);
+        file.modifiedDate = new Date(file.modifiedDate);
+
+        fileService.saveFile(file, function(err) {
+            if (err) return next(new restify.InternalError("Could not save the file"));
+
+            res.send({
+                status: 'success',
+                file: file
+            });
+
+            return next();
+        });
+    }
+});
+
+/**
  * DELETE /files/:entryId
  * Version: 1
  */
